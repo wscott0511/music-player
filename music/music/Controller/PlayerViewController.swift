4bda07e6e6ea263447418a087de15fae24533807
Ryan Scott <57239286+wscott0511@users.noreply.github.com>
2020-09-01 10:35:07 -0700

    Getting Started

---
+++ music/music/Controller/PlayerViewController.swift
@@ -0,0 +1,34 @@
+//
+//  PlayerViewController.swift
+//  music
+//
+//  Created by Ryan Scott on 8/28/20.
+//  Copyright © 2020 Ryan Scott. All rights reserved.
+//
+
+import UIKit
+
+class PlayerViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var albumTitleImage: UIImageView!
+    @IBOutlet weak var songTitleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
